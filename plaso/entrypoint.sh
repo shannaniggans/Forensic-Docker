9133a624ca787d1b933364c90a6dae34f47afff6
Shanna Daly <shanna.daly@neptune.paraflare.net>
2020-11-11 11:53:54 +1100

    updated

--- plaso/entrypoint.sh
+++ plaso/entrypoint.sh
@@ -1,19 +1,44 @@
 #!/bin/sh
-rm /data/output.*
+yes | cp -rf /data/json_line.py /usr/lib/python3/dist-packages/plaso/output/json_line.py
+rm /data/test/output.*
 
-## Collect and parse browser and user artefacts to timeline
-log2timeline.py --artifact-filters BrowserHistory,BrowserCache --parser webhist --partitions all --no_vss --workers ${WORKER_NUM:-2} --buffer_size 2048M --profiling_sample_rate 10000 --status_view none /data/test/output-browser.plaso /data/*flat.vmdk &&
-psort.py -o json -w /data/test/test-browser.json /data/test/output-browser.plaso "date > '2020-11-01 00:00:00' AND date < '2020-11-08 00:00:00'"
+## Collect and parse eventlogs - evtx
+log2timeline.py --artifact-filters WindowsEventLogs --parser winevtx --partitions all --no_vss --workers ${WORKER_NUM:-2} --buffer_size 2048M --profiling_sample_rate 10000 /data/test/output-evtx.plaso /data/*flat.vmdk &&
 
-## Collect Windows artefacts first
-log2timeline.py --artifact-filters WindowsEventLogs,WindowsXMLEventLogTerminalServices,WindowsFirewallLogFile,WindowsUserJumpLists,WindowsUserRecentFiles,WindowsUserRegistryFiles,WindowsAMCacheHveFile,WindowsAppCompatCache,WindowsPrefetchFiles,WindowsRecycleBin,WindowsScheduledTasks --partitions all --parser win7,winreg,esedb/srum,winjob,prefetch,olecf --no_vss --workers ${WORKER_NUM:-2} --buffer_size 2048M --profiling_sample_rate 10000 --status_view none /data/test/output-win.plaso /data/*flat.vmdk
+##adjust t he timeframe you want to analyse
+psort.py -o json_line -w /data/test/output-evtx.json /data/test/output-evtx.plaso "date > '2020-11-03 00:00:00' AND date < '2020-11-07 00:00:00'" &&
 
-psort.py -o l2tcsv -w /data/test/test-win.csv /data/test/output-win.plaso "date > '2020-11-01 00:00:00' AND date < '2020-11-08 00:00:00'"
+## To do: make the brackets insert 1st character and last.
+(echo "["; cat /data/test/output-evtx.json; echo "]") >evtx.json &&
 
 ## Collect and parse mft
-log2timeline.py --artifact-filters NTFSMFTFiles --parser mft --partitions all --no_vss --workers ${WORKER_NUM:-2} --buffer_size 2048M --profiling_sample_rate 10000 --status_view none /data/test/output-mft.plaso /data/*flat.vmdk &&
-psort.py -o l2tcsv -w /data/test/test-mft.csv /data/test/output-mft.plaso "date > '2020-11-01 00:00:00' AND date < '2020-11-08 00:00:00'"
+log2timeline.py --artifact-filters NTFSMFTFiles --parser mft --partitions all --no_vss --workers ${WORKER_NUM:-2} --buffer_size 2048M --profiling_sample_rate 10000 /data/test/output-mft.plaso /data/*flat.vmdk &&
 
+psort.py -o json_line -w /data/test/output-mft.json /data/test/output-mft.plaso "date > '2020-11-03 00:00:00' AND date < '2020-11-07 00:00:00'" &&
 
+## To do: make the brackets insert 1st character and last.
+(echo "["; cat /data/test/output-mft.json; echo "]") >mft.json &&
 
-## 
+## Collect registry artefacts into output.plaso
+log2timeline.py --artifact-filters WindowsRegistryFilesAndTransactionLogs --partitions all --parser winreg --no_vss --workers ${WORKER_NUM:-2} --buffer_size 2048M --profiling_sample_rate 10000 /data/test/output-winreg.plaso /data/*flat.vmdk &&
+
+psort.py -o json_line -w /data/test/output-winreg.json /data/test/output-winreg.plaso "date > '2020-11-03 00:00:00' AND date < '2020-11-07 00:00:00'" &&
+
+## To do: make the brackets insert 1st character and last.
+(echo "["; cat /data/test/output-winreg.json; echo "]") >winreg.json &&
+
+## Collect win7 parser without win_gen artefacts into output.plaso
+log2timeline.py --file-filter filter_windows.yaml --partitions all --parser win7,!win_gen --no_vss --workers ${WORKER_NUM:-2} --buffer_size 2048M --profiling_sample_rate 10000 /data/test/output-win7.plaso /data/*flat.vmdk &&
+
+psort.py -o json_line -w /data/test/output-win7.json /data/test/output-win7.plaso "date > '2020-11-03 00:00:00' AND date < '2020-11-07 00:00:00'" &&
+
+## To do: make the brackets insert 1st character and last.
+(echo "["; cat /data/test/output-win7.json; echo "]") >win7.json &&
+
+## Collect win_gen parser without win_gen artefacts into output.plaso
+log2timeline.py --file-filter filter_windows.yaml --partitions all --parser win_gen,!winreg --no_vss --workers ${WORKER_NUM:-2} --buffer_size 2048M --profiling_sample_rate 10000 /data/test/output-win_gen.plaso /data/*flat.vmdk &&
+
+psort.py -o json_line -w /data/test/output-win_gen.json /data/test/output-win_gen.plaso "date > '2020-11-03 00:00:00' AND date < '2020-11-07 00:00:00'" &&
+
+## To do: make the brackets insert 1st character and last.
+(echo "["; cat /data/test/output-win_gen.json; echo "]") >win_gen.json
